2952d8a22c333f8f8b1e707d8fdc7689f72a5784
vishnu <vishnuvyreddy@gmail.com>
2023-06-11 10:14:16 +0530

    added valid username and password

--- src/main/java/com/example/Inmar/inmar/InmarApplication.java
+++ src/main/java/com/example/Inmar/inmar/InmarApplication.java
@@ -27,7 +27,7 @@ public class InmarApplication {
 
 	@PostConstruct
 	public void initUsers() {
-		List<User> users = Stream.of(new User(101, "javatechie", "password", "javatechie@gmail.com"),
+		List<User> users = Stream.of(new User(101, "vishnu", "password", "vishnu@gmail.com"),
 				new User(102, "user1", "pwd1", "user1@gmail.com"), new User(103, "user2", "pwd2", "user2@gmail.com"),
 				new User(104, "user3", "pwd3", "user3@gmail.com")).collect(Collectors.toList());
 		repository.saveAll(users);
